e4aeb2e53f0d12e82f9876be5f1bc5990a2d5fa5
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-01-09 14:50:48 +0100

    feat(findAllOccurrences): add solution for findAllOccurrences task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -71,8 +71,9 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const array = arr.filter((element) => (element === item ? arr.push() : 0));
+  return array.length;
 }
 
 /**
